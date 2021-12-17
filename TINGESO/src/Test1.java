d1fc2737025b584bee60a201e531a03f687af256
matias <matias.jaque@usach.cl>
2021-12-17 00:46:04 -0300

    Auto stash before merge of "main" and "origin/main"

--- TINGESO/src/Test1.java
+++ TINGESO/src/Test1.java
@@ -2,16 +2,12 @@
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.JavascriptExecutor;
+
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-//Contacto exitoso
 public class Test1{
     private WebDriver driver;
     private Map<String, Object> vars;
@@ -28,27 +24,101 @@ public class Test1{
         TimeUnit.SECONDS.sleep(5);
         driver.quit();
     }
+    // ENCONTRAR UN ARTICULO DE LA TIENDA FILTRANDO EN BUSQUEDA
     @Test
-    public void Test1() {
+    public void a() throws InterruptedException {
         driver.get("http://automationpractice.com/index.php");
-        driver.manage().window().setSize(new Dimension(1936, 1056));
-        driver.findElement(By.linkText("Contact us")).click();
-        driver.findElement(By.id("id_contact")).click();
-        {
-            WebElement dropdown = driver.findElement(By.id("id_contact"));
-            dropdown.findElement(By.xpath("//option[. = 'Customer service']")).click();
+        try {
+            Thread.sleep(1000);
         }
-        driver.findElement(By.cssSelector(".form-group:nth-child(5)")).click();
-        driver.findElement(By.id("email")).click();
-        driver.findElement(By.id("email")).sendKeys("mingeso@gmail.com");
-        driver.findElement(By.id("id_order")).click();
-        driver.findElement(By.id("id_order")).sendKeys("141414");
-        driver.findElement(By.id("message")).click();
-        driver.findElement(By.id("message")).sendKeys("Problema");
-        driver.findElement(By.cssSelector("#submitMessage > span")).click();
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.manage().window().setSize(new Dimension(1552, 832));
+        try {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.id("search_query_top")).click();
+        try {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.id("search_query_top")).sendKeys("blou");
+        try {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.id("search_query_top")).sendKeys(Keys.ENTER);
+        try {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
     }
     @Test
+<<<<<<< Updated upstream
     public void untitled() {
 
     }
 }
+=======
+
+    public void b() {
+        driver.get("http://automationpractice.com/index.php");
+        try {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.manage().window().setSize(new Dimension(1552, 832));
+        try {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.cssSelector("#homefeatured > .ajax_block_product:nth-child(6) .button:nth-child(2) > span")).click();
+        try {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.id("thumb_17")).click();
+        try {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.cssSelector(".fancybox-item")).click();
+        try {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.id("thumb_19")).click();
+        try {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.cssSelector(".fancybox-item")).click();
+    }
+
+
+}
+
+
+>>>>>>> Stashed changes
